1a33995e24afd4200aa4cbfca1c16d210c44e965
hotbreakb <areyhappy12@gmail.com>
2022-04-25 13:50:50 +0900

    add install storybook

---
+++ .storybook/main.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "stories": [
+    // "../stories/**/*.stories.mdx",
+    // "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/components/**/*",
+    "../src/stories/**/*.stories.*",
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions"
+  ],
+  "framework": "@storybook/react"
+}
